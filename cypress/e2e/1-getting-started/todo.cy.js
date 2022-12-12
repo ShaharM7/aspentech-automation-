66a84f25844cd3ec5bc9959695d488f5741b8c4f
Shahar Moshe Refaeli <shaharrefaeli61@gmail.com>
2022-12-12 20:36:10 +0200

    finish the test and fixing the ci job

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,15 +1,23 @@
+/// <reference types= "cypress"/>
+import { slowCypressDown } from 'cypress-slow-down'
+Cypress.on('uncaught:exception', (err) => {
+    return false
+})
 
 describe('Navigate to ', () => {
-  beforeEach(() => {
-    cy.visit('')
-  })
-
-  it('select random fruits', () => {
-    cy.contains('span', 'orange').then((button) => {
-      console.log(button.text())
+    beforeEach(() => {
+        cy.visit('')
     })
-    cy.contains('span', 'apple ')
-    cy.contains('span', 'cherry ')
-  })
+
+    it('select random fruits', () => {
+        slowCypressDown()
+        cy.contains('span', 'orange').should('be.visible').parent().parent().parent()
+            .find('input')
+            .uncheck({force: true})
+        cy.contains('span', 'apple').should('be.visible').parent().parent().parent()
+            .find('input').check()
+        cy.contains('span', 'cherry').should('be.visible').parent().parent().parent()
+            .find('input').check()
+    })
 
 })
